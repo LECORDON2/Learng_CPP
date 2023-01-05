c6aee7a7c2d74cab789c36f92b3e43c06ffd67eb
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-01-05 20:41:57 +0900

    Update 11D Linked_List.cpp

--- 11D Linked_List.cpp
+++ 11D Linked_List.cpp
@@ -13,11 +13,25 @@ int main()
 
 	InitList(&list);
 
+		
+	PushFront_Me(&list, 1);
+	PushFront_Me(&list, 2);
+	PushFront_Me(&list, 3);
+	PushFront_Me(&list, 4);
+	PushFront_Me(&list, 5);
+
 	PushBack(&list, 100);
 	PushBack(&list, 200);
 	PushBack(&list, 300);
 	PushBack(&list, 400);
 
+	PushFront(&list, 1);
+	PushFront(&list, 2);
+	PushFront(&list, 3);
+	PushFront(&list, 4);
+	PushFront(&list, 5);
+
+	
 	ReleaseList(&list);
 
 	return 0;
@@ -53,6 +67,7 @@ void InitList(tLinkedList* _pList);
 // 연결형 리스트 데이터 추가
 void PushBack(tLinkedList* _pList, int _iData);
 void PushFront(tLinkedList* _pList, int iData);
+void PushFront_Me(tLinkedList* _pList, int iData);
 
 // 연결형 리스트 메모리 해제
 void ReleaseList(tLinkedList* _pIist);
@@ -127,6 +142,44 @@ void PushFront(tLinkedList* _pList, int iData)
 //	Release(_pList->pHaedNode);
 //}
 
+void PushFront(tLinkedList* _pList, int _iData)
+{
+	// 새로 생성시킨 노드의 다음을 기존의 헤드로 지정한다.
+	tNode* pNewNode = (tNode*)malloc(sizeof(tNode));
+	pNewNode->iData = _iData;
+	pNewNode->pNextNode = _pList->pHaedNode;
+	
+	// 리스트의 헤드노드 포인터를 갱신한다.
+	_pList->pHaedNode = pNewNode;
+
+	// 데이터 카운트 증가
+	++_pList->iCount;
+}
+
+// 과제 만들기
+void PushFront_Me(tLinkedList* _pList, int _iData)
+{
+	// 1. 새로운 노드와 데이터 추가하기
+	tNode* pNode = (tNode*)malloc(sizeof(tNode));
+	pNode->iData = _iData; 	
+	
+	// 2. 처음 데이터인지 체크
+	if (_pList->iCount == 0)
+	{
+		_pList->pHaedNode = pNode;
+		pNode->pNextNode = nullptr;
+	}
+	else
+	{
+		// 3. 추가 되어지는 노드의 넥스트 노드는 이전의 헤드노드의 주소가 들어가 있어야 함
+		pNode->pNextNode = _pList->pHaedNode;
+		
+		// 4. 리스트의 헤드노드가 되어야함
+		_pList->pHaedNode = pNode;
+	}
+	++_pList->iCount;
+}
+
 void ReleaseList(tLinkedList* _pList)
 {
 	tNode* pDeleteNode = _pList->pHaedNode;
@@ -138,3 +191,10 @@ void ReleaseList(tLinkedList* _pList)
 		pDeleteNode = pNext;
 	}
 }
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Statu

5877ce4bee9d0c79c547b271f7f6ce727bc4e138
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-02-18 20:24:57 +0900

    Create CScene.cpp

---
+++ Windows_API.cpp/26D Scene_Manager/CScene.cpp
@@ -0,0 +1,40 @@
+#include "pch.h"
+#include "CScene.h"
+#include "CObject.h"
+
+CScene::CScene()
+{
+}
+
+CScene::~CScene()
+{
+	for (UINT i = 0; i < (UINT)GROUP_TYPE::END; ++i)
+	{
+		for (size_t j = 0; j < m_arrObj[i].size(); ++j)
+			// m_arrObj[i] 그룹 벡터의 j 물체 삭제
+			// vector의 삭제는 vector가 있는 클래스 소멸자에서 한다.
+			delete m_arrObj[i][j];
+	}
+}
+
+void CScene::update()
+{
+	for (UINT i = 0; i < (UINT)GROUP_TYPE::END; ++i)
+	{
+		for (size_t j = 0; j < m_arrObj[i].size(); ++j)
+		{
+			m_arrObj[i][j]->update();
+		}
+	}
+}
+
+void CScene::render(HDC _dc)
+{
+	for (UINT i = 0; i < (UINT)GROUP_TYPE::END; ++i)
+	{
+		for (size_t j = 0; j < m_arrObj[i].size(); ++j)
+		{
+			m_arrObj[i][j]->render(_dc);
+		}
+	}
+}

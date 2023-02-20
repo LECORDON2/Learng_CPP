f21e49ea31ff5fe7bd1f9009e2939e743d787b41
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-02-20 23:38:01 +0900

    Create define.h

---
+++ Windows_API.cpp/27D Object/define.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "pch.h"
+
+// singleton 매크로
+#define SINGLE(type) public: static type* Getinst() { static type mgr; return &mgr; } private: type(); ~type();
+
+#define FDT CTimeMgr::Getinst()->GetfDT()
+#define DT CTimeMgr::Getinst()->GetDT()
+
+#define KEY_CHECK(key, state) CKeyMgr::Getinst()->GeyKEyState(key) == state
+#define KEY_HOLD(key) KEY_CHECK(key, KEY_STATE::HOLD)
+#define KEY_TAP(key) KEY_CHECK(key, KEY_STATE::TAP)
+#define KEY_AWAY(key) KEY_CHECK(key, KEY_STATE::AWAY)
+#define KEY_NONE(key) KEY_CHECK(key, KEY_STATE::NONE)
+
+enum class GROUP_TYPE
+{
+	DEFAULT,
+	PLAYER,
+	MISSILE,
+	MONSTER,
+
+	END = 32,
+};
+
+enum class SCENE_TYPE
+{
+	TOOL,
+	START,
+	STAGE_01,
+	STAGE_02,
+
+	END,
+};

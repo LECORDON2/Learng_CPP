0225d6be61d19a9371ac72123b659a97b5ef8e80
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-02-20 23:33:09 +0900

    Create CCore.h

---
+++ Windows_API.cpp/27D Object/CCore.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class CCore
+{
+	SINGLE(CCore);
+
+private:
+	HWND	m_hWnd;          // 메인 윈도우 핸들
+	POINT	m_ptResolution; // 메인 윈도우 해상도
+	HDC		m_hDc;          // 메인 윈도우에 Draw할 DC
+
+	HBITMAP m_hBit;
+	HDC		m_memDC;
+
+
+public:
+	int init(HWND _hWnd, POINT _ptResolution);
+	void progress();
+
+private:
+	void update();
+	void render();
+
+public:
+	HWND GetMainHwnd() { return m_hWnd; }
+	POINT GetResolution() { return m_ptResolution; }
+};

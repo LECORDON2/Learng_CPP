6e8425b132a158985c0f5fd1f11793106a17db33
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-02-20 23:33:46 +0900

    Create CMissile.h

---
+++ Windows_API.cpp/27D Object/CMissile.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "CObject.h"
+
+class CMissile :
+    public CObject
+{
+private:
+    float m_fDir; // 위 아래 방향
+
+public:
+    void SetDir(bool _bup)
+    {
+        if (_bup)
+        {
+            m_fDir = -1.f;
+        }
+        else
+        {
+            m_fDir = 1.f;
+        }
+    }
+
+public:
+    virtual void update();
+    virtual void render(HDC _dc);
+
+public:
+    CMissile();
+    ~CMissile();
+};

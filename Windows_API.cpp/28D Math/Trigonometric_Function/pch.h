f006dea838f836725695c8031a20b40aab325b75
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-02-24 14:36:49 +0900

    Create pch.h

---
+++ Windows_API.cpp/28D Math/Trigonometric_Function/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// 미리 컴파일된 헤더
+#include <Windows.h>
+#include <iostream>
+#include <string>
+using std::string;
+using std::wstring;
+#include <vector>
+using std::vector;
+
+#include "define.h"
+#include "struct.h"
+#include <math.h>

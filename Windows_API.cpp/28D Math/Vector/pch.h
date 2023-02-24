7453494aef4c33f2276d504e886c0221aea58824
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-02-24 14:38:20 +0900

    Create pch.h

---
+++ Windows_API.cpp/28D Math/Vector/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// 미리 컴파일된 헤더
+#include <Windows.h>
+#include <iostream>
+#include <string>
+using std::string;
+using std::wstring;
+#include <vector>
+using std::vector;
+
+#include <assert.h>
+#include "define.h"
+#include "struct.h"
+#include <math.h>

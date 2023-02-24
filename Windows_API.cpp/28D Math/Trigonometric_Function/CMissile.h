4335392ed888b4fe7d07bac382b9b45686e7d867
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-02-24 14:35:39 +0900

    Create CMissile.h

---
+++ Windows_API.cpp/28D Math/Trigonometric_Function/CMissile.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "CObject.h"
+
+class CMissile :
+    public CObject
+{
+private:
+    float m_fTheta; // 이동방향
+
+public:
+    void SetDir(float _fTheta) { m_fTheta = _fTheta; }
+
+public:
+    virtual void update();
+    virtual void render(HDC _dc);
+
+public:
+    CMissile();
+    ~CMissile();
+};

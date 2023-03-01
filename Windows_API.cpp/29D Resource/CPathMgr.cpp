20c01ed9d4be572957a31227cd6f0be60aac1581
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-01 21:37:07 +0900

    Create CPathMgr.cpp

---
+++ Windows_API.cpp/29D Resource/CPathMgr.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "CPathMgr.h"
+#include "CCore.h"
+
+CPathMgr::CPathMgr()
+	: m_szContentPath{}
+{}
+
+CPathMgr::~CPathMgr()
+{}
+
+void CPathMgr::init()
+{
+	// 현재 디렉토리를 기억하는 윈도우 함수
+	GetCurrentDirectory(255, m_szContentPath);
+
+	int iLen = wcslen(m_szContentPath);
+
+	// 상위폴더로
+	for (int i = iLen - 1; 0 <= i; --i)
+	{
+		if (m_szContentPath[i] == '\\')
+		{
+			m_szContentPath[i] = '\0';
+			break;
+		}
+	}
+
+	// content폴더로 가기위해 문자열 이어 붙이기
+	wcscat_s(m_szContentPath, 255, L"\\bin\\content\\");
+
+	// + bin\\content\\
+	// 프로젝트 경로가 나오게 된다.
+	// 속성 -> 디버깅 에서 작업 디렉토리를 바꾸어 주면 된다.
+	// SetWindowText(CCore::Getinst()->GetMainHwnd(), m_szContentPath);
+}

1e2c068bfe83db68d805f6418d75b01fb57abb83
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-01 21:40:50 +0900

    Create CTexture.h

---
+++ Windows_API.cpp/29D Resource/CTexture.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "CRes.h"
+class CTexture :
+    public CRes
+{
+private:
+    HDC         m_dc;
+    HBITMAP     m_hBit;
+    BITMAP      m_bitInfo; // 정보를 받아올 구조체
+
+public:
+    void Load(const wstring& _strFilePath);
+    UINT Width() { return m_bitInfo.bmWidth; }
+    UINT Height() { return m_bitInfo.bmHeight; }
+    HDC GetDC() { return m_dc; }
+
+public:
+    CTexture();
+    ~CTexture();
+};

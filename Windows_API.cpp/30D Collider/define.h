507b6b8b336bb96cd72ea6d1ae2c62272a1f8492
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-03 22:47:58 +0900

    Create define.h

---
+++ Windows_API.cpp/30D Collider/define.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "pch.h"
+
+// singleton 매크로
+#define SINGLE(type) public: static type* Getinst() { static type mgr; return &mgr; } private: type(); ~type();
+
+#define FDT CTimeMgr::Getinst()->GetfDT()
+#define DT CTimeMgr::Getinst()->GetDT()
+
+#define KEY_CHECK(key, state) CKeyMgr::Getinst()->GeyKEyState(key) == state
+#define KEY_HOLD(key) KEY_CHECK(key, KEY_STATE::HOLD)
+#define KEY_TAP(key) KEY_CHECK(key, KEY_STATE::TAP)
+#define KEY_AWAY(key) KEY_CHECK(key, KEY_STATE::AWAY)
+#define KEY_NONE(key) KEY_CHECK(key, KEY_STATE::NONE)
+
+#define PI 3.1415926535f
+
+enum class GROUP_TYPE
+{
+	DEFAULT,
+	PLAYER,
+	MISSILE,
+	MONSTER,
+
+	END = 32,
+};
+
+enum class SCENE_TYPE
+{
+	TOOL,
+	START,
+	STAGE_01,
+	STAGE_02,
+
+	END,
+};
+
+enum class BRUSH_TYPE
+{
+	HOLLOW,
+
+	END,
+};
+
+enum class PEN_TYPE
+{
+	RED,
+	GREEN,
+	BLUE,
+
+	END,
+};

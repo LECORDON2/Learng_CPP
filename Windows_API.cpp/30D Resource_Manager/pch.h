355f8d171e4e7596dd258c9c849704ed1e7afd8b
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-03 22:42:07 +0900

    Create pch.h

---
+++ Windows_API.cpp/30D Resource_Manager/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// 미리 컴파일된 헤더
+#include <Windows.h>
+#include <iostream>
+#include <string>
+using std::string;
+using std::wstring;
+#include <vector>
+using std::vector;
+
+#include <map>
+using std::map;
+using std::make_pair;
+
+#pragma comment(lib, "Msimg32.lib")
+
+#include <assert.h>
+#include "define.h"
+#include "struct.h"
+#include <math.h>

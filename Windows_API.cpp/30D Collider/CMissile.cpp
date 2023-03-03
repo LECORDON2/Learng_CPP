f839c563ba2d48fe184b353bb6752f2fca9d3bb1
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-03 22:44:25 +0900

    Create CMissile.cpp

---
+++ Windows_API.cpp/30D Collider/CMissile.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "CMissile.h"
+#include "CTimeMgr.h"
+
+CMissile::CMissile()
+	: m_fTheta(PI / 4.f)
+	, m_vDir(Vec2(1.f, 1.f))
+{
+	m_vDir.Normalize();
+	CreateCollider();
+}
+
+CMissile::~CMissile()
+{
+}
+
+void CMissile::update()
+{
+	Vec2 vPos = GetPos();
+
+	//vPos.y -= 600.f * sinf(m_fTheta) * FDT;
+	//vPos.x += 600.f * cosf(m_fTheta) * FDT;
+
+	vPos.y += 600.f * m_vDir.y * FDT;
+	vPos.x += 600.f * m_vDir.x * FDT;
+
+	SetPos(vPos);
+}
+
+void CMissile::render(HDC _dc)
+{
+	Vec2 vPos = GetPos();
+	Vec2 vScale = GetScale();
+	Ellipse(_dc, (int)(vPos.x - vScale.x / 2.f), (int)(vPos.y - vScale.y / 2.f)
+		         , (int)(vPos.x + vScale.x / 2.f), (int)(vPos.y + vScale.y / 2.f));
+}

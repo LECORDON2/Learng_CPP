c18aec4471a6105e73994b8a3117d639d87d107a
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-07 20:44:01 +0900

    Create CScene_Start.h

---
+++ Windows_API.cpp/30D Collider/CScene_Start.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "CScene.h"
+class CScene_Start :
+    public CScene // 상속 받음
+{
+
+public:
+    virtual void Enter(); //  virtual 은 쓰지않아도 되나 명시적으로 보여주기 위해서 적어준다.
+    virtual void Exit();
+
+public:
+    CScene_Start();
+    ~CScene_Start();
+};

75f8feddfe33bc940ef661026435f49a0ffeeb40
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-12 21:22:43 +0900

    Create CObject.cpp

---
+++ Windows_API.cpp/32D Scene & Object/CObject.cpp
@@ -0,0 +1,63 @@
+#include "pch.h"
+#include "CObject.h"
+#include "CKeyMgr.h"
+#include "CTimeMgr.h"
+#include "CCollider.h"
+
+CObject::CObject()
+	: m_vPos{}
+	, m_vScale{}
+	, m_pCollider(nullptr)
+	, m_bAlive(true)
+{
+}
+
+CObject::CObject(const CObject& _origin)
+	: m_strName(_origin.m_strName)
+	, m_vPos(_origin.m_vPos)
+	, m_vScale(_origin.m_vScale)
+	, m_pCollider(nullptr)
+	, m_bAlive(true)
+{
+	// CCollider에 있는 복사생성자 활용
+	m_pCollider = new CCollider(*_origin.m_pCollider);
+	m_pCollider->m_pOwner = this;
+}
+
+CObject::~CObject()
+{
+	if (nullptr != m_pCollider)
+	{
+		delete m_pCollider;
+	}
+}
+
+void CObject::CreateCollider()
+{
+	m_pCollider = new CCollider;
+	m_pCollider->m_pOwner = this;
+}
+
+void CObject::finalupdate()
+{
+	if (m_pCollider)
+	{
+		m_pCollider->finalupdate();
+	}
+}
+
+void CObject::render(HDC _dc)
+{
+	Rectangle(_dc, (int)(m_vPos.x - m_vScale.x / 2.f), (int)(m_vPos.y - m_vScale.y / 2.f)
+	             , (int)(m_vPos.x + m_vScale.x / 2.f), (int)(m_vPos.y + m_vScale.y / 2.f));
+
+	component_render(_dc);
+}
+
+void CObject::component_render(HDC _dc)
+{
+	if (m_pCollider != nullptr)
+	{
+		m_pCollider->render(_dc);
+	}
+}

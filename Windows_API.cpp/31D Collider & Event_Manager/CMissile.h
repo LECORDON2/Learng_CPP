4306dbfa752b749d221802a812231888cc98b2bd
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-12 21:15:41 +0900

    Create CMissile.h

---
+++ Windows_API.cpp/31D Collider & Event_Manager/CMissile.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "CObject.h"
+
+class CMissile :
+    public CObject
+{
+private:
+    float m_fTheta; // 이동방향
+    Vec2  m_vDir;
+
+public:
+    void SetDir(float _fTheta) { m_fTheta = _fTheta; }
+    void SetDir(Vec2 _vdir) 
+    { 
+        m_vDir = _vdir; 
+        m_vDir.Normalize();
+    }
+
+public:
+    virtual void update();
+    virtual void render(HDC _dc);
+
+public:
+    virtual void OnCollisionEnter(CCollider* _pOther);
+
+public:
+    CMissile();
+    ~CMissile();
+};

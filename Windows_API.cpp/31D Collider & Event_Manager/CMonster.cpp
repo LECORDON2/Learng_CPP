381cc0eefec4914f2413134c6beb82908b8e3074
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-12 21:16:07 +0900

    Create CMonster.cpp

---
+++ Windows_API.cpp/31D Collider & Event_Manager/CMonster.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include "CMonster.h"
+#include "CTimeMgr.h"
+#include "CCollider.h"
+
+CMonster::CMonster()
+	: m_vCenterPos(Vec2(0.f, 0.f))
+	, m_fSpeed(100.f)
+	, m_fMaxDistance(50.f)
+	, m_iDir(1)
+	, m_iHP(5)
+{
+	CreateCollider();
+	GetCollider()->SetScale(Vec2(40.f, 40.f));
+}
+
+CMonster::~CMonster()
+{
+}
+
+void CMonster::update()
+{
+	Vec2 vCurpos = GetPos();
+
+	// 진행 방향으로 시간당 m_fSpeed 만큼 이동
+	vCurpos.x += FDT * m_fSpeed * m_iDir;
+
+	// 배회 거리 기준량을 넘어섰는지 확인
+
+	float fDist = abs(m_vCenterPos.x - vCurpos.x) - m_fMaxDistance;
+
+	if (0.f < fDist)
+	{
+		// 방향 변경
+		m_iDir *= -1;
+		vCurpos.x += fDist * m_iDir;
+	}
+	SetPos(vCurpos);
+}
+
+void CMonster::OnCollisionEnter(CCollider* _pOther)
+{
+	CObject* pOtherObj = _pOther->GetObj();
+
+	if (pOtherObj->GetName() == L"Missile_Player")
+	{
+		m_iHP -= 1;
+
+		if (m_iHP =< 0)
+		{
+			DeleteObject(this);
+		}
+	}
+}

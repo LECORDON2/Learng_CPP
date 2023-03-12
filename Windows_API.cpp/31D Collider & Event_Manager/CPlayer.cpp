42cf70b613147b249021faa4fbd06ac36e3c157c
LECORDON2 <104762750+LECORDON2@users.noreply.github.com>
2023-03-12 21:17:45 +0900

    Create CPlayer.cpp

---
+++ Windows_API.cpp/31D Collider & Event_Manager/CPlayer.cpp
@@ -0,0 +1,87 @@
+#include "pch.h"
+#include "CPlayer.h"
+#include "CKeyMgr.h"
+#include "CTimeMgr.h"
+#include "CMissile.h"
+#include "CSceneMgr.h"
+#include "CScene.h"
+#include "CPathMgr.h"
+#include "CResMgr.h"
+#include "CTexture.h"
+#include "CCollider.h"
+
+CPlayer::CPlayer()
+	: m_pTex(nullptr)
+{
+	m_pTex = CResMgr::Getinst()->LoadTexture(L"PlaterTex", L"texture\\snake.bmp");
+
+	CreateCollider();
+	GetCollider()->SetOffsetPos(Vec2(0.f, 0.f));
+	GetCollider()->SetScale(Vec2(25.f, 25.f));
+}
+
+CPlayer::~CPlayer()
+{
+}
+
+void CPlayer::update()
+{
+	Vec2 vPos = GetPos();
+
+	if (KEY_HOLD(KEY::W))
+	{
+		vPos.y -= 200.f * FDT;
+	}
+	if (KEY_HOLD(KEY::S))
+	{
+		vPos.y += 200.f * FDT;
+	}
+	if (KEY_HOLD(KEY::A))
+	{
+		vPos.x -= 200.f * FDT;
+	}
+	if (KEY_HOLD(KEY::D))
+	{
+		vPos.x += 200.f * FDT;
+	}
+
+	if (KEY_TAP(KEY::SPACE))
+	{
+		CreateMissile();
+	}
+
+	SetPos(vPos);
+}
+
+void CPlayer::render(HDC _dc)
+{
+	int iWidth = (int)m_pTex->Width();
+	int iHeight = (int)m_pTex->Height();
+
+	Vec2 vPos = GetPos();
+
+	TransparentBlt(_dc, int(vPos.x - (float)(iWidth / 2))
+		, int(vPos.y - (float)(iHeight / 2))
+		, iWidth, iHeight
+		, m_pTex->GetDC()
+		, 0, 0, iWidth, iHeight
+	    , RGB(255, 0 , 255)); // 투명 처리할 RGB값
+
+	// 컴포넌트(충돌체, etc...) 가 있는 경우 렌더
+	component_render(_dc);
+}
+
+void CPlayer::CreateMissile()
+{
+	Vec2 vMissilePos = GetPos();
+	vMissilePos.y -= GetScale().y / 2.f;
+
+	// Missile Object
+	CMissile* pMissile = new CMissile;
+	pMissile->SetName(L"Missile_Player");
+	pMissile->SetPos(vMissilePos);
+	pMissile->SetScale(Vec2(25.f, 25.f));
+	pMissile->SetDir(Vec2(0.f, -1.f));
+
+	CreateObject(pMissile, GROUP_TYPE::PROJ_PLAYER);
+}
